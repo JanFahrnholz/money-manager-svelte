16492be0090bacc61ae21a261b8593ce9b2e5c09
Jan <jf@ambimax.de>
2023-08-25 11:39:37 +0200

    add production db depending on env

--- src/pocketbase.ts
+++ src/pocketbase.ts
@@ -1,6 +1,11 @@
 import PocketBase from "pocketbase";
 
-const client = new PocketBase("https://pb-staging.industed.com");
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://pb.industed.com"
+    : "https://pb-staging.industed.com";
+
+const client = new PocketBase(url);
 client.autoCancellation(false);
 
 const clientId = client.authStore.model?.id;
