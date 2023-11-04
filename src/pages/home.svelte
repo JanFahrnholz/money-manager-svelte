036f19e0d9dd440fe7c4d42de6ababa7cfdf1c52
Jan <jf@ambimax.de>
2023-11-04 23:20:56 +0100

    add gauge statistics display

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -3,21 +3,23 @@
     BlockTitle,
     List,
     ListItem,
-    Page,
-    useStore,
+    useStore
   } from "framework7-svelte";
   import PlannedTransactionList from "../planned-transactions/components/planned-transaction-list.svelte";
+  import TransactionGauge from "../statistics/components/transaction-gauge.svelte";
   import store from "../store";
   import TransactionList from "../transactions/components/transaction-list.svelte";
 
   let user = useStore(store, "user", (value) => (user = value));
 </script>
 
-<BlockTitle>Your account</BlockTitle>
+<BlockTitle>your account</BlockTitle>
 <List strong inset dividers class="margin-bottom-half">
   <ListItem title="Balance" after={`${user?.balance}€`} />
   <ListItem title="Your ID" after={`${user?.id}`} />
 </List>
 
+<BlockTitle>transactions</BlockTitle>
+<TransactionGauge />
 <PlannedTransactionList />
 <TransactionList />
---
+++ src/statistics/components/transaction-gauge.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { Block, Gauge, f7, useStore } from "framework7-svelte";
+  import TransactionStatistics from "../transaction-statistics";
+  import TransactionStatisticsOptions from "./transaction-statistics-options.svelte";
+  let percentage;
+  let type;
+  let total;
+  let statistics;
+  let transactions = useStore("transactions", (value) => {
+    statistics = new TransactionStatistics(value.items);
+    transactions = value;
+    refresh();
+  });
+
+  const refresh = () => {
+    if (!statistics) return;
+
+    percentage = statistics.getPercentage();
+    total = statistics.getTotalAmount();
+    type = TransactionStatistics.type;
+  };
+</script>
+
+<Block class="text-align-center">
+  <Gauge
+    type="circle"
+    value={percentage / 100}
+    size={200}
+    borderWidth={10}
+    borderBgColor={f7.colors.primary}
+    valueText={`${percentage || "0"}%`}
+    valueFontSize={41}
+    valueTextColor={f7.colors.primary}
+    labelText={`total of ${total || "0"}€`}
+  />
+</Block>
+<TransactionStatisticsOptions
+  defaultDateRange={30}
+  defaultType="Income"
+  on:refresh={refresh}
+/>
