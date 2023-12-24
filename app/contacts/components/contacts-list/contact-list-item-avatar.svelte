60772a3404a1fd7297e297e28ee7fed6e44a60d1
Jan <jf@ambimax.de>
2023-12-24 01:30:40 +0100

    fix avatar props warning

--- app/contacts/components/contacts-list/contact-list-item-avatar.svelte
+++ app/contacts/components/contacts-list/contact-list-item-avatar.svelte
@@ -1,16 +1,18 @@
-<script lang="ts">
-  import Avatar from "svelte-avatar";
+<script>
+    import Avatar from "svelte-avatar";
 
-  export let contact;
+    export let contact;
 
-  let avatarColor = "#ffd600";
-  if (contact.balance < 0) avatarColor = "#ff3b30";
-  if (contact.balance > 0) avatarColor = "#4cd964";
+    let avatarColor = "#ffd600";
+    if (contact.balance < 0) avatarColor = "#ff3b30";
+    if (contact.balance > 0) avatarColor = "#4cd964";
 </script>
 
 <Avatar
-  name={contact.name}
-  bgColor={avatarColor}
-  textColor="black"
-  size="40px"
+        src=""
+        initials=""
+        name={contact.name}
+        bgColor={avatarColor}
+        textColor="black"
+        size="40px"
 />
