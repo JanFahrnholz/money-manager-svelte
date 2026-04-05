f18417a5ce1b390d3a7525819b5e64a303871551
Jan Fahrnholz <jm.fahrnholz@gmail.com>
2026-04-06 01:50:11 +0200

    fix(app2): add CAMERA permission and WebView getUserMedia support for Android

--- app2/android/app/src/main/java/com/moneymanager/app/MainActivity.java
+++ app2/android/app/src/main/java/com/moneymanager/app/MainActivity.java
@@ -1,5 +1,21 @@
 package com.moneymanager.app;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Allow WebView to request camera permission for getUserMedia
+        this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
